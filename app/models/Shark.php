cc98ccad4ab735f6145eea3b9fe3ffafba40068f
bea <beatriztattooer@gmail.com>
2023-08-06 09:04:53 -0300

    :construction: wip: Tamagotchi and Shark type
    - Class Tamagotchi
    - Class for Shark Type
    - Commented stuff
    
    It ain't much but it's honest job :)

---
+++ app/models/Shark.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'Tamagotchi.php';
+
+class Shark extends Tamagotchi {
+
+    public string $specie;
+
+    public function __construct($name, $color, $specie){
+        $this->name = $name;
+        $this->color = $color;
+        $this->specie = $specie;
+    }
+
+    public function intro() {
+        echo "Wow! Your Tamagotchi was born! Its name is {$this->name}, its color is {$this->color}, and its specie is {$this->specie} :D";
+    }
+}
+
+$shark = new Shark("Shark", "White", "Hammer");
+$shark->intro();
+?>
---
+++ public/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Projeto Integrador - 2023</title>
+    </head>
+
+    <body>
+        <?php
+            include '../app/models/Shark.php';
+        ?>
+
+        <!--
+            TODO: frontend stuff
+            <button>Health</button>
+            <button>Food</button>
+            <button>Toilet</button>
+            <button>Games</button>
+            <button>Frems</button>
+
+            TODO: tamagotchi life cicle
+            <div id="tamagotchi"></div>
+            
+            <button>Study</button>
+            <button>Medicine</button>
+            <button>Lights</button>
+            <button>Attention</button>
+            <button>Frems List</button>
+        -->
+
+    </body>
+
+</html>
